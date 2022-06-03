2b09d713d0af2daf24187bcae12f9e4b20a3a243
Nour <nkhoudp@etech-nyc.org>
2022-06-03 15:30:41 -0400

    action+

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,7 @@ public class Game {
   private Grid grid;
   private int userRow;
   private int userCol;
-  private int totalTime;
+  private int totalTimeLeft;
   private int msElapsed;
   private int timesGet;
   private int timesAvoid;
@@ -20,7 +20,7 @@ public class Game {
     grid = new Grid(5, 8);
     userRow = 3;
     userCol = 6;
-    totalTime = 15;
+    totalTimeLeft = 15;
     msElapsed = 0;
     timesGet = 0;
     timesAvoid = 0;
@@ -34,6 +34,9 @@ public class Game {
     while (!isGameOver()) {
       grid.pause(100);
       handleKeyPress();
+
+      action();
+
       if (msElapsed % 300 == 0) {
         scrollLeft();
         populateRightEdge();
@@ -47,7 +50,10 @@ public class Game {
 
     //check last key pressed
     int key = grid.checkLastKeyPressed();
-    System.out.println(key);
+    if(key != -1){
+      System.out.println(key);
+      System.out.println(new Location(userRow,userCol));
+    }
 
     //set "w" key to move the plane up
     if(key == 87 && userRow != 0){
@@ -123,8 +129,23 @@ if(key == 65   && userRow != 0){
   }
 
   public void action(){
-    if(userRow == 1 && userCol == 1){
-      totalTime--;
+    if(userRow == 3 && userCol == 6){
+      //System.out.println("strange room");
+      grid.showMessageDialog("You find yourself in a strange room...");
+      userRow--;
+      
+      //shift the user picture up in the array
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
+  grid.setImage(new Location(3,6), null);
+      // Location oldLoc = new Location(userRow, userCol+1);
+      // grid.setImage(oldLoc, null);
+    }
+
+    if(userRow == 3 && userCol == 5){
+      totalTimeLeft--;
+      grid.showMessageDialog("You find a note. It says, \"You have 15 minutes to find the key.\"");
+
     }
 
   }
@@ -141,12 +162,12 @@ if(key == 65   && userRow != 0){
 
   }
   
-  public int getScore() {
-    return 0;
+  public int getTimeLeft() {
+    return this.totalTimeLeft;
   }
   
   public void updateTitle() {
-    grid.setTitle("Game:  " + getScore());
+    grid.setTitle("Time Left:  " + getTimeLeft());
   }
   
   public boolean isGameOver() {
