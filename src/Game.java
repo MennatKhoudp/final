079afac3b2216a9a57376e75b257d5b34517b9aa
Nour <nkhoudp@etech-nyc.org>
2022-06-08 14:18:35 -0400

    updates

--- src/Game.java
+++ src/Game.java
@@ -8,6 +8,7 @@ public class Game {
   private Grid grid;
   private int userRow;
   private int userCol;
+  private int key;
   private int totalTimeLeft;
   private int msElapsed;
   private int timesGet;
@@ -21,6 +22,7 @@ public class Game {
     userRow = 3;
     userCol = 6;
     totalTimeLeft = 15;
+    key = 0;
     msElapsed = 0;
     timesGet = 0;
     timesAvoid = 0;
@@ -39,17 +41,10 @@ public class Game {
 
       action();
 
-      // if(isTimeUp()){
-      //   //do something???
-      // }
-
-      updateUserLoc();
-
-
-      // if (msElapsed % 300 == 0) {
-      //   scrollLeft();
-      //   populateRightEdge();
-      // }
+      if (msElapsed % 300 == 0) {
+        scrollLeft();
+        populateRightEdge();
+      }
       updateTitle();
       msElapsed += 100;
     }
@@ -60,222 +55,286 @@ public class Game {
     //check last key pressed
     int key = grid.checkLastKeyPressed();
     if(key != -1){
-      System.out.print("Key: " + key);
-      System.out.println("\tLoc: " + new Location(userRow,userCol));
+      System.out.println(key);
+      System.out.println(new Location(userRow,userCol));
     }
 
     //set "w" key to move the plane up
     if(key == 87 && userRow != 0){
-      //check case where out of bounds
-      if (userRow != grid.getNumRows()-1){
+        //check case where out of bounds
+        if (userRow != grid.getNumRows()-1){
           
-      }
-      //change the field for userrow
-      Location oldLoc = new Location(userRow+1, userCol);
-      grid.setImage(oldLoc, null);
+        }
+        //change the field for userrow
 
-    }
+        userRow--;
 
-    //if I push "s", then plane goes down
-    if(key == 83   && userRow != 0){
-      if (userRow != grid.getNumRows()-1){
+        //shift the user picture up in the array
+        Location loc = new Location(userRow, userCol);
+        grid.setImage(loc, userPic);
         
-      }
-      
-      //change the field for userrow
-      userRow++;
+        Location oldLoc = new Location(userRow+1, userCol);
+        grid.setImage(oldLoc, null);
 
-      //shift the user picture up in the array
-      Location loc = new Location(userRow, userCol);
-      grid.setImage(loc, userPic);
-      
-      Location oldLoc = new Location(userRow-1, userCol);
-      grid.setImage(oldLoc, null);
-    }
+  }
+    //if I push "s", then plane goes down
+      if(key == 83   && userRow != 0){
+        if (userRow != grid.getNumRows()-1){
+          
+        }
+        //change the field for userrow
 
-    //if I push "d", then plane goes down
-    if(key == 68   && userRow != 0){
-      if (userCol != grid.getNumCols()-1){
-              
-      }
-      //change the field for usercol
+        userRow++;
 
-      userCol++;
-      //shift the user picture up in the array
-      Location loc = new Location(userRow, userCol);
-      grid.setImage(loc, userPic);
+
+        //shift the user picture up in the array
+        Location loc = new Location(userRow, userCol);
+        grid.setImage(loc, userPic);
+        
+        Location oldLoc = new Location(userRow-1, userCol);
+        grid.setImage(oldLoc, null);
+  }
+//
+//if I push "d", then plane goes down
+if(key == 68   && userRow != 0){
+  if (userCol != grid.getNumCols()-1){
+          
+  }
+  //change the field for usercol
+
+  userCol++;
+
+
+  //shift the user picture up in the array
+  Location loc = new Location(userRow, userCol);
+  grid.setImage(loc, userPic);
   
-      Location oldLoc = new Location(userRow, userCol-1);
-      grid.setImage(oldLoc, null);
-    }
+  Location oldLoc = new Location(userRow, userCol-1);
+  grid.setImage(oldLoc, null);
+}
 
-    if(key == 65   && userRow != 0){
-      if (userCol != grid.getNumCols()-1){
-              
-      }
-      //change the field for usercol
-      userCol--;
-
-      //shift the user picture up in the array
-      Location loc = new Location(userRow, userCol);
-      grid.setImage(loc, userPic);
-      
-      Location oldLoc = new Location(userRow, userCol+1);
-      grid.setImage(oldLoc, null);
-    }
-
+if(key == 65   && userRow != 0){
+  if (userCol != grid.getNumCols()-1){
+          
   }
+  //change the field for usercol
 
-  //method to check if time is up
-  public boolean isTimeUp(){
-    if(totalTimeLeft == 0 || totalTimeLeft < 0){
-      grid.showMessageDialog("Time Up! Game Over!");
-      return true;
-    }
-    return false;
-  }
+  userCol--;
 
-  //method to update the user's location
-  public void updateUserLoc(){
-    Location userLoc = new Location(userRow, userCol);
-    grid.setImage(userLoc, userPic);
+
+  //shift the user picture up in the array
+  Location loc = new Location(userRow, userCol);
+  grid.setImage(loc, userPic);
+  
+  Location oldLoc = new Location(userRow, userCol+1);
+  grid.setImage(oldLoc, null);
+}
+
   }
 
   public void action(){
-
     //user entrance
     if(userRow == 3 && userCol == 6){
-      System.out.println("strange room entered");
+      //System.out.println("strange room");
       grid.showMessageDialog("You find yourself in a strange room...");
+      userRow--;
       
       //shift the user picture up in the array
-      userRow--;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(3,6), null);
     }
     
     //note
     if(userRow == 3 && userCol == 5){
       totalTimeLeft--;
-      grid.showMessageDialog("You find a note. It says, \"You have 15 minutes to find the key.\"");
+      grid.showMessageDialog("You find a note. It says, \"You have 15 minutes to find the key to escape!\"");
+      userRow--;
 
       //shift the user picture up in the array
-      userRow--;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(3,5), null);
     }
 
     //couch
     if(userRow == 3 && userCol == 8){
-      System.out.println("Comfy Couch");
-      grid.showMessageDialog("That looks comfy... you lay on the couch and accidentally fall asleep");
+      //System.out.println("strange room");
+      grid.showMessageDialog("That looks comfy... you lay on the couch and accidentally fall asleep. You lost a lot of time.");
       totalTimeLeft-=15;
-
-      //shift the user picture up in the array
-      userCol--;
-      grid.setImage(new Location(3,8), null);
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
+        userCol--;
+      
+        //shift the user picture up in the array
+        Location loc = new Location(userRow, userCol);
+        grid.setImage(loc, userPic);
+        grid.setImage(new Location(3,8), null);
+      }
     }
 
     //table 1
     if(userRow == 4 && userCol == 8){
-      
-      System.out.println("Table 1");
-      grid.showMessageDialog("You look around the table and find something under it... It's a clue.");
-      grid.showMessageDialog("It says,\" \" ");
+      //System.out.println("strange room");
+      grid.showMessageDialog("You look around the table and find something under it... It's a riddle!");
+      grid.showMessageDialog("It says,\"I hold things with spines. I am a home of knowledge.\"");
       totalTimeLeft--;
-
-      //shift the user picture up in the array
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
       userCol--;
+      
+      //shift the user picture up in the array
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(4,8), null);
+      }
     }
 
     //arm stand
     if(userRow == 5 && userCol == 7){
-      System.out.println("Arm Stand");
-      grid.showMessageDialog("");
-      totalTimeLeft-=15;
-
-      //shift the user picture up in the array
+      //System.out.println("strange room");
+      grid.showMessageDialog("You look inside the coat pockets... Nothing here.");
+      totalTimeLeft--;
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
       userRow--;
+      
+      //shift the user picture up in the array
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(5,7), null);
     }
+  }
 
     //book shelf
     if(userRow == 1 && userCol == 7){
-      System.out.println("Book Shelf");
-      grid.showMessageDialog("");
-      totalTimeLeft-=15;
-
-      //shift the user picture up in the array
+      //System.out.println("strange room");
+      grid.showMessageDialog("You look in between the books, one of them have a missing cover...");
+      totalTimeLeft-=3;
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
       userRow++;
+      
+      //shift the user picture up in the array
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(1,7), null);
     }
+  }
 
     //chair
     if(userRow == 5 && userCol == 5){
-      System.out.println("Chair");
-      grid.showMessageDialog("");
-      totalTimeLeft-=15;
-
-      //shift the user picture up in the array
+      //System.out.println("strange room");
+      grid.showMessageDialog("That looks comfy... you relax on the couch. You lost a lot of time.");
+      totalTimeLeft-=5;
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
       userRow--;
+      
+      //shift the user picture up in the array
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(5,5), null);
     }
+  }
 
     //big cabinet
     if(userRow == 1 && userCol == 5){
-      System.out.println("Big Cabinet");
-      grid.showMessageDialog("");
-      totalTimeLeft-=15;
- 
-      //shift the user picture up in the array
+      //System.out.println("strange room");
+      grid.showMessageDialog("You look inside and find a book cover!");
+      grid.showMessageDialog("The back says, \"The key is where time flys.\"");
+      totalTimeLeft-=5;
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
       userRow++;
+      
+      //shift the user picture up in the array
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(1,5), null);
     }
+  }
 
     //clock
     if(userRow == 2 && userCol == 4){
-      System.out.println("Clock");
-      grid.showMessageDialog("");
-      totalTimeLeft-=15;
+      //System.out.println("strange room");
+      grid.showMessageDialog("You found the key!");
+      totalTimeLeft-=2;
+      key++;
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
+      userRow--;
       
       //shift the user picture up in the array
-      userRow--;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(2,4), null);
     }
+  }
 
     //table 2
     if(userRow == 5 && userCol == 1){
-      System.out.println("Table 2");
-      grid.showMessageDialog("");
+      //System.out.println("strange room");
+      grid.showMessageDialog("You look around the table... Nothing here.");
       totalTimeLeft-=15;
-     
-      //shift the user picture up in the array
+      if(totalTimeLeft == 0 || totalTimeLeft < 0){
+        grid.showMessageDialog("Time's Up!");
+        grid.showMessageDialog("Game Over.");
+      }else{
       userRow--;
+      
+      //shift the user picture up in the array
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(5,1), null);
     }
+  }
 
     //stairs (win/lose)
     if(userRow == 3 && userCol == 2){
-      System.out.println("Stairs W/L");
-      grid.showMessageDialog("");
-      
+      //System.out.println("strange room");
+      if(key == 1){
+        grid.showMessageDialog("You found the key!");
+
+      }else{
+        grid.showMessageDialog("You need a key.");
+        userRow--;
+
       //shift the user picture up in the array
-      userRow--;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
       grid.setImage(new Location(3,2), null);
+      }
+      
     }
 
   }
   
-  // public void populateRightEdge(){
+  public void populateRightEdge(){
 
-  // }
+  }
   
-  // public void scrollLeft(){
+  public void scrollLeft(){
 
-  // }
+  }
   
-  // public void handleCollision(Location loc) {
+  public void handleCollision(Location loc) {
 
-  // }
+  }
   
   public int getTimeLeft() {
     return this.totalTimeLeft;
